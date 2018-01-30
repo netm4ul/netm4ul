f2b85299d4d8df1ae200d1436e7724b3d4626b2c
Unknown <l.marchal91@gmail.com>
2018-01-30 17:05:23 +0100

    Exec dig
    Try on root-me challenge

--- modules/recon/dns_bruteforce/dns_bruteforce.go
+++ modules/recon/dns_bruteforce/dns_bruteforce.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("DNS Bruteforce Module")
+	args := []string{"@212.129.38.224", "-p", "54011", "ch11.challenge01.root-me.org", "-t", "AXFR"}
+	fmt.Println("Args :", args)
+	cmd := exec.Command("/usr/bin/dig", args...)
+	fmt.Println("My cmd :", cmd)
+	res, err := cmd.Output()
+	if err != nil {
+        fmt.Println("error occured")
+        panic(err)
+    }
+	fmt.Println("\nRes :\n",string(res))
 }
