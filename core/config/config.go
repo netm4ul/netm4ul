db8cbf34ba0e3e89586546477e2685b1f016359f
Edznux <edznux@gmail.com>
2018-04-10 17:07:56 +0200

    Fix config parsing & tests

--- core/config/config.go
+++ core/config/config.go
@@ -86,17 +86,16 @@ var Config ConfigToml
 func LoadConfig(file string) {
 	var configPath string
 
-	ex, err := os.Executable()
-	if err != nil {
-		panic(err)
-	}
-
-	path := filepath.Dir(ex)
-
 	if file == "" {
-		configPath = filepath.Join(path, "netm4ul.conf")
+		dir, err := os.Getwd()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		configPath = filepath.Join(dir, "netm4ul.conf")
 	} else {
-		configPath = filepath.Join(path, file)
+		configPath = file
 	}
 
 	if _, err := toml.DecodeFile(configPath, &Config); err != nil {
