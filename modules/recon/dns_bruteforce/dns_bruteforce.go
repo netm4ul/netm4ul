39d0416191fc7186edf159ca0b6bc3f82509ae7f
Skawak <l.marchal91@gmail.com>
2018-04-11 10:12:16 -0400

    Structure of module Dns_bruteforce
    Not working

--- modules/recon/dns_bruteforce/dns_bruteforce.go
+++ modules/recon/dns_bruteforce/dns_bruteforce.go
@@ -1,20 +1,70 @@
-package main
+package dns_bf
 
 import (
 
 	"fmt"
-	"os/exec"
+	"github.com/OJ/gobuster/libgobuster"
+
 )
 
-func main() {
-	args := []string{"@212.129.38.224", "-p", "54011", "ch11.challenge01.root-me.org", "-t", "AXFR"}
-	fmt.Println("Args :", args)
-	cmd := exec.Command("/usr/bin/dig", args...)
-	fmt.Println("My cmd :", cmd)
-	res, err := cmd.Output()
+//NewDns generate a new Dns module (type modules.Module)
+func NewDns_BF() modules.Module {
+	gob.Register(DnsResult{}) // change var ?
+	var d modules.Module
+	d = Dns_bf{}
+	return d
+}
+
+// Name : name getter
+func (D Dns_bf) Name() string {
+	return "Dns_BF"
+}
+
+// Author : Author getter
+func (D Dns_bf) Author() string {
+	return "Skawak"
+}
+
+// Version : Version  getter
+func (D Dns_bf) Version() string {
+	return "0.1"
+}
+
+// DependsOn : Generate the dependencies requirement
+func (D Dns_bf) DependsOn() []modules.Condition {
+	var _ modules.Condition
+	return []modules.Condition{}
+}
+
+// Run : Main function of the module
+func (D Dns_bf) Run(data []string) (modules.Result, error) {
+
+	// Let's go
+
+}
+
+// ParseConfig : Load the config from the config folder
+func (D Dns_bf) ParseConfig() error {
+	ex, err := os.Executable()
 	if err != nil {
-        fmt.Println("error occured")
-        panic(err)
-    }
-	fmt.Println("\nRes :\n",string(res))
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+	configPath := filepath.Join(exPath, "config", "dns_bf.conf")
+
+	if _, err := toml.DecodeFile(configPath, &D.Config); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+
+WriteDb : Save data
+func (D Dns_bf) WriteDb(result modules.Result, mgoSession *mgo.Session, projectName string) error {
+	log.Println("Write to the database.")
+	var data DnsResult // change var ?
+	data = result.Data.(DnsResult) // change var ?
+
+	raw := bson.M{projectName + ".results." + result.Module: data}
+	database.UpsertRawData(mgoSession, projectName, raw)
+	return nil
 }
