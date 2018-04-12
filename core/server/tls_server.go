d72e13413fbe4246ff9912a165a4d1a66b215e5a
bytemare <d@bytema.re>
2018-04-12 04:23:53 +0200

    Added curve preferences and other minor changes

--- core/server/tls_server.go
+++ core/server/tls_server.go
@@ -5,20 +5,32 @@ import (
 	"io/ioutil"
 	"log"
 	"crypto/x509"
+	"github.com/netm4ul/netm4ul/cmd/colors"
 )
 
 // Build the TLS configuration for server
 func TLSBuildServerConf() (tls.Config){
 
+	caCert := "certificates/Netm4ul_CA.crt"
+	publKey := "certificates/Netm4ul_Server.crt"
+	privKey := "certificates/Netm4ul_Server.pem"
+
 	// Read CA file and initialise
-	certBytes, err := ioutil.ReadFile("certificates/NetM4ul_CA.crt")
+	certBytes, err := ioutil.ReadFile(caCert)
 	if err != nil {
-		log.Fatalln("Unable to read localhost.pem", err)
+		log.Println(colors.Red("Unable to read %s : %s"), caCert, err.Error())
 	}
 
 	clientCertPool := x509.NewCertPool()
 	if ok := clientCertPool.AppendCertsFromPEM(certBytes); !ok {
-		log.Fatalln("Unable to add certificate to certificate pool")
+		log.Println(colors.Red("Unable to add CA certificate to certificate pool"))
+	}
+
+	// Read own KeyPair
+	cert, err := tls.LoadX509KeyPair(publKey, privKey)
+	if err != nil {
+		log.Println(err)
+		log.Println(colors.Red("Unable to read X509KeyPair : %s"), err.Error())
 	}
 
 	tlsConfig := tls.Config{
@@ -32,6 +44,10 @@ func TLSBuildServerConf() (tls.Config){
 		PreferServerCipherSuites: true,
 		// TLS 1.2 because we can
 		MinVersion: tls.VersionTLS12,
+		// Server Key Material
+		Certificates: []tls.Certificate{cert},
+		// Set preferences for used curves (but certs should already have been made)
+		CurvePreferences: []tls.CurveID{ tls.X25519, tls.CurveP256, tls.CurveP384},
 	}
 
 	return tlsConfig
