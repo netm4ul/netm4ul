7d64e339ffce47756413c3e3d75c09d5372cdf20
bytemare <d@bytema.re>
2018-04-12 03:51:24 +0200

    Added function to build TLS configuration with (for now) hardcoded parameters and dummy certificates

--- core/server/tls_server.go
+++ core/server/tls_server.go
@@ -1,1 +1,38 @@
 package server
+
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"log"
+	"crypto/x509"
+)
+
+// Build the TLS configuration for server
+func TLSBuildServerConf() (tls.Config){
+
+	// Read CA file and initialise
+	certBytes, err := ioutil.ReadFile("certificates/NetM4ul_CA.crt")
+	if err != nil {
+		log.Fatalln("Unable to read localhost.pem", err)
+	}
+
+	clientCertPool := x509.NewCertPool()
+	if ok := clientCertPool.AppendCertsFromPEM(certBytes); !ok {
+		log.Fatalln("Unable to add certificate to certificate pool")
+	}
+
+	tlsConfig := tls.Config{
+		// Reject any TLS certificate that cannot be validated
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		// Ensure that we only use our "CA" to validate certificates
+		ClientCAs: clientCertPool,
+		// PFS because we can
+		CipherSuites: []uint16{tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256},
+		// Force it server side
+		PreferServerCipherSuites: true,
+		// TLS 1.2 because we can
+		MinVersion: tls.VersionTLS12,
+	}
+
+	return tlsConfig
+}
