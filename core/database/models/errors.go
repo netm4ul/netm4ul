d7fffebe2841500974c7caf61654cbf494c4892c
Edznux <edznux@gmail.com>
2018-10-29 22:27:46 +0100

    Add userAlready exist error

--- core/database/models/errors.go
+++ core/database/models/errors.go
@@ -7,6 +7,10 @@ import (
 // ErrNotFound is the internal representation used by the adapters to express a item was not found. (Instead of returning an empty <type>)
 var ErrNotFound error
 
+//UserAlreadyExist is the error code when there is an attempt to register an already existing account
+var ErrUserAlreadyExist error
+
 func init() {
 	ErrNotFound = errors.New("Not found")
+	ErrUserAlreadyExist = errors.New("User already exist")
 }
