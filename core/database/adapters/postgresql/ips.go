4e5bf2f5eb5298a5b33fd5b4f322f52ef7c186f8
Edznux <edznux@gmail.com>
2018-11-26 07:07:10 +0100

    Add raw query for ips

--- core/database/adapters/postgresql/ips.go
+++ core/database/adapters/postgresql/ips.go
@@ -12,23 +12,16 @@ import (
 func (pg *PostgreSQL) createOrUpdateIP(projectName string, ip pgIP) error {
 	log.Debugf("Inserting ip : %+v", ip)
 
-	proj, err := pg.getProject(projectName)
-	if err != nil {
-		return errors.New("Could not find corresponding project for ip :" + err.Error())
-	}
-
-	ip.ProjectID = proj.ID
-
-	res := pg.db.
-		Where("project_id = ?", proj.ID).
-		Where("value = ?", ip.Value).
-		Where("network = ?", ip.Network).
-		FirstOrCreate(&ip)
-
+	var foundIP pgIP
+	res := pg.db.Raw(selectIPByProjectName, projectName, ip.Value).Scan(&foundIP)
 	if res.Error != nil {
-		return errors.New("Could not save ip in the database :" + res.Error.Error())
+		return errors.New("Could not find the ip in the database :" + res.Error.Error())
 	}
-	return nil
+	// not found, create
+	if foundIP.ID == 0 {
+		return pg.createIP(projectName, ip)
+	}
+	return pg.updateIP(projectName, ip)
 }
 
 //CreateOrUpdateIP is the public wrapper to create or update a new IP in the database.
@@ -56,15 +49,46 @@ func (pg *PostgreSQL) createOrUpdateIPs(projectName string, ips []pgIP) error {
 	return nil
 }
 
+func (pg *PostgreSQL) createIP(projectName string, ip pgIP) error {
+	//TOFIX ?
+	res := pg.db.Exec(insertIP, projectName, ip.Value, ip.Network)
+	if res.Error != nil {
+		return res.Error
+	}
+	return nil
+}
+
 //CreateIP is the public wrapper to create a new IP in the database.
 func (pg *PostgreSQL) CreateIP(projectName string, ip models.IP) error {
+	// convert to pgIP first
+	pip := pgIP{}
+	pip.FromModel(ip)
+	err := pg.createIP(projectName, pip)
+	if err != nil {
+		return err
+	}
+
 	events.NewEventIP(ip)
-	return errors.New("Not implemented yet")
+	return nil
+}
+
+func (pg *PostgreSQL) updateIP(projectName string, ip pgIP) error {
+	res := pg.db.Model(&ip).Update(ip)
+	if res.Error != nil {
+		return res.Error
+	}
+	return nil
 }
 
 //UpdateIP is the public wrapper to update a new IP in the database.
 func (pg *PostgreSQL) UpdateIP(projectName string, ip models.IP) error {
-	return errors.New("Not implemented yet")
+	pip := pgIP{}
+	pip.FromModel(ip)
+	err := pg.updateIP(projectName, pip)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 //CreateOrUpdateIPs is the public wrapper to create or update multiple IP
@@ -88,12 +112,13 @@ func (pg *PostgreSQL) CreateOrUpdateIPs(projectName string, ips []models.IP) err
 func (pg *PostgreSQL) getIPs(projectName string) ([]pgIP, error) {
 
 	pgips := []pgIP{}
-	res := pg.db.Raw(`
-	SELECT ips.* FROM "ips","projects"
-		WHERE "ips"."deleted_at" IS NULL
-		AND ips.project_id = projects.id
-		AND ((projects.name = ?))
-	`, projectName).Scan(&pgips)
+	res := pg.db.Raw(selectIPsByProjectName, projectName).Scan(&pgips)
+	// res := pg.db.Raw(`
+	// SELECT ips.* FROM "ips","projects"
+	// 	WHERE "ips"."deleted_at" IS NULL
+	// 	AND ips.project_id = projects.id
+	// 	AND ((projects.name = ?))
+	// `, projectName).Scan(&pgips)
 
 	if res.Error != nil {
 		return nil, errors.New("Could not get IPs : " + res.Error.Error())
